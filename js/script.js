24737d0c3da974f4f24ccdd5df63b61476a42d3b
Farhan Sadik <kazifarhan121@gmail.com>
2024-08-08 12:16:09 +0600

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,10 @@
+// JS Example
+function showText() {
+	document.getElementById('text').innerHTML="Hello World!";
+}
+function showText_2() {
+	document.getElementById('text_2').style.fontSize="36px";
+}
+function hideText() {
+	document.getElementById('text_3').style.display="none";
+}
